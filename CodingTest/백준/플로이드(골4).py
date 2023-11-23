911612bf26d4023b50fc72cd436066aba34e2279
ho-taek <kht072623@naver.com>
2023-11-23 16:04:28 +0900

    Update 플로이드(골4).py

--- CodingTest/백준/플로이드(골4).py
+++ CodingTest/백준/플로이드(골4).py
@@ -6,9 +6,7 @@ INF = int(1e9)
 graph = [[INF]*(n+1) for _ in range(n+1)]
 
 for a in range(1,n+1):
-    for b in range(1,n+1):
-        if a == b:
-            graph[a][b]= 0
+    graph[a][a] = 0
 
 for _ in range(m):
     a,b,c = map(int,input().split())
