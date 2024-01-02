52c99a66bad4a2a587411e9febbe10f176d002f6
ho-taek <kht072623@naver.com>
2024-01-02 21:34:07 +0900

    Update 최빈수 구하기(D2).py

--- CodingTest/SWAcademy/파이썬/최빈수 구하기(D2).py
+++ CodingTest/SWAcademy/파이썬/최빈수 구하기(D2).py
@@ -1,11 +1,13 @@
-n = int(input())
+t = int(input())
 
-for i in range(n):
-    a = int(input())
+for i in range(1,n+1):
+    n,m = map(int, input().split())
+    if len(n) > len(m):
+    minLi = 
     li = list(map(int, input().split()))
     answer = [0]*101
     result = 0
     for i in li:
         answer[i] += 1
-        if answer[i] > result : result = answer[i]
+        if answer[i] >= answer[result] : result = i
     print("#%d"%a, result)
