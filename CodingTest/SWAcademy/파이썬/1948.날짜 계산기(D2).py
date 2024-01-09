09a67aa4bee4556ea13d576b6b26fbe6a8cee1a8
ho-taek <kht072623@naver.com>
2024-01-09 20:32:21 +0900

    Create 1948.날짜 계산기(D2).py

---
+++ CodingTest/SWAcademy/파이썬/1948.날짜 계산기(D2).py
@@ -0,0 +1,7 @@
+T = int(input())
+li = [0,31,59,90,120,151,181,212,243,273,304,334,365]
+for t in range(1,T+1):
+    a,b,c,d = map(int, input().split())
+    value = (li[c]+d) - (li[a]+b) +1
+    print("#%d"%t, value)
+
