71fe96ca4194c19ecf584c8ae1376a38fd163322
ho-taek <kht072623@naver.com>
2024-01-17 23:41:15 +0900

    Update 연습.java

--- CodingTest/연습.java
+++ CodingTest/연습.java
@@ -1,8 +1,17 @@
-class Sample{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args){
+class 연습{
 
-        int a = 5;
+    public static void main(String[] args) throws IOException{
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       String str = br.readLine();
+       StringTokenizer strTokenizer = new StringTokenizer(str);
+
+       System.out.println(str);
 
     }
 }
