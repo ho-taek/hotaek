c8a75ea117a71ffb39e063c2977976b7abb09621
ho-taek <kht072623@naver.com>
2024-01-19 00:08:42 +0900

    Create _2751.java

---
+++ CodingTest/백준/java/실버/_2751.java
@@ -0,0 +1,26 @@
+package 백준.java.실버;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.*;
+
+public class _2751 {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        
+       ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for(int i=0 ; i < n; i++){
+            list.add(Integer.parseInt(br.readLine()));
+         }
+         Collections.sort(list);
+         for(int i : list){
+            System.out.println(i);
+         }
+    }
+}
