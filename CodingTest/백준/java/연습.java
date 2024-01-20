9e5904909385268d8a21f771fe01884c73a7c68c
ho-taek <kht072623@naver.com>
2024-01-20 16:09:38 +0900

    Update 연습.java

--- CodingTest/백준/java/연습.java
+++ CodingTest/백준/java/연습.java
@@ -1,18 +1,26 @@
 package 백준.java;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.util.Arrays;
 
 class 연습{
 
-    public static void main(String[] args) throws IOException{
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+       int[] array = {2,3,5,1,0,4};
 
-       String str = br.readLine();
-       StringTokenizer strTokenizer = new StringTokenizer(str);
-
-       System.out.println(str);
-
+       for(int i=0; i<array.length-1; i++){
+        
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+            int tmp = array[i];
+            for(int j=i; j<(array.length-1); j++){
+                if(array[j] < min){
+                    index = j;
+                    min = array[j];
+                }
+            }
+            array[i] = array[index];
+            array[index] = tmp;   
+       }
+       System.out.println(Arrays.toString(array));
     }
 }
