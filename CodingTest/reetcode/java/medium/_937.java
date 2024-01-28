fe76be76655a1e92a8bc2c8fb7a72d91d65ad9c2
ho-taek <kht072623@naver.com>
2024-01-28 15:06:56 +0900

    _937.java

---
+++ CodingTest/reetcode/java/medium/_937.java
@@ -0,0 +1,47 @@
+package reetcode.java.medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class _937 {
+
+    public static void main(String[] args){
+
+    }
+
+    public static String[] reorderLogFiles(String[] logs) {
+        List<String> letterLogs = new ArrayList<String>();
+        List<String> digitLogs = new ArrayList<String>(); 
+
+        // Int 배열일때
+        for(String log : logs){
+            if(Character.isDigit(log.split(" ")[1].charAt(0))){
+                digitLogs.add(log);
+            }else{
+                letterLogs.add(log);
+            }
+        }
+
+        //문자배열 재배치
+        Collections.sort(letterLogs, new Comparator<String>(){
+
+            @Override
+            public int compare(String o1, String o2) {
+                String[] s1x = o1.split(" ", 2);
+                String[] s2x = o2.split(" ", 2);
+
+                int compared = s1x[1].compareTo(s2x[1]);
+                if(compared == 0){
+                    return s1x[0].compareTo(s2x[0]);
+                }
+                return compared;
+            }
+
+        });
+
+        return logs;
+    }
+    
+}
--- CodingTest/reetcode/연습.java
+++ CodingTest/reetcode/연습.java
@@ -1,5 +1,18 @@
 package reetcode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class 연습 {
     
+    public static void main(String[] args){
+
+        Integer[] array = {3,5,2,1,4};
+
+        String a = "id1";
+        String b = "id3";
+
+        System.out.println(a.compareTo(b));
+        
+    }
 }
