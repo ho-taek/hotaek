cc573d0044a7947ef34dcac444bcc747b2efc1a2
ho-taek <kht072623@naver.com>
2024-02-01 00:18:12 +0900

    _11659.java

--- CodingTest/reetcode/java/easy/_21.java
+++ CodingTest/reetcode/java/easy/_21.java
@@ -22,15 +22,16 @@ public class ListNode {
     }
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode node = new ListNode();
-        
-        while(list1.next != null || list2.next != null){
-            if(list1.val > list2.val){
-                node.val = list2.val;
-            }
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
 
+        if (list1.val < list2.val){
+            list1.next = mergetTwoLists(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = mergetTwoLists(list1, list2.next);
+            return list2;
         }
-
     }
 }
 
---
+++ CodingTest/백준/java/실버/_11659.java
@@ -0,0 +1,39 @@
+package 백준.java.실버;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _11659 {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int [] array = new int[n+1];
+        String[] input = br.readLine().split(" ");
+        int result = 0;
+        for(int i =0 ; i < n ; i++){
+            result += Integer.parseInt(input[i]);
+            array[i+1] = result;
+        }
+        System.out.println("result" + Arrays.toString(array));
+        for(int i=0; i < m; i++){
+            String[] check = br.readLine().split(" ");
+            int a = Integer.parseInt(check[0]);
+            int b = Integer.parseInt(check[1]);
+
+            if(a == 1){
+                System.out.println("답a"+array[b]);
+            }else{
+                System.out.println("답"+(array[b]-array[a-1]));
+            }
+        }
+
+    }
+}
