b6467e41d44a2414b5fa78b23d2e4b8fa542dd55
ho-taek <kht072623@naver.com>
2024-02-07 23:30:12 +0900

    Update 연습.java

--- CodingTest/reetcode/연습.java
+++ CodingTest/reetcode/연습.java
@@ -4,17 +4,8 @@ import java.util.*;
 public class 연습 {
     
     public static void main(String[] args){
-        Queue<Integer> queue = new LinkedList<Integer>();
+        StringBuilder sb = new StringBuilder("abcd");
 
-        queue.add(3);
-        queue.add(5);
-        queue.add(7);
-
-        System.out.println(queue);
-        System.out.println(queue.peek());
-        queue.remove();
-        System.out.println(queue);
-        System.out.println(queue.poll());
-        
+        System.out.println(sb.substring(2));
     }
 }
