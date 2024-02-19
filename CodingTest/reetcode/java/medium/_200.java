966f1e84b5f1cc161d05bb32daba99e700cbdb24
ho-taek <kht072623@naver.com>
2024-02-19 21:31:18 +0900

    Update _200.java

--- CodingTest/reetcode/java/medium/_200.java
+++ CodingTest/reetcode/java/medium/_200.java
@@ -1,8 +1,56 @@
 package reetcode.java.medium;
 
+import java.util.LinkedList;
+import java.util.*;
+
 public class _200 {
+    public static boolean[][] visited;
+
+    public class Node{
+        int x,y;
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+    }
+
+    public static void main(String[] args){
+
+    }
     
-    public int numIslands(char[][] grid) {
+    public static int numIslands(char[][] grid) {
+        visited = new boolean[grid.length][grid.length];
+
+
         
+    
+        return 0;
+    }
+
+    public static void bfs(Node start, int size){
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(start);
+
+
+        int[] dx = {0,0,-1,1};
+        int[] dy = {-1,1,0,0};
+
+        while(true){
+            Node q = queue.poll();
+            visited[q.x][q.y] = true;
+            for(int i=0; i < 4; i++){
+                int nx = q.x + dx[i];
+                int ny = q.y + dy[i];
+    
+                if(nx >=0 && nx < size && ny >=0 && ny < size ){
+                    if(visited[nx][ny]){
+                        queue.offer(new Node(nx, ny));
+                    }
+                }
+            }
+        }
+
+   
     }
 }
