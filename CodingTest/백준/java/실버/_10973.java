5a346c68c7b138776f72da5484f8029b324bcef7
ho-taek <kht072623@naver.com>
2024-02-22 00:22:47 +0900

    n과 m

---
+++ CodingTest/백준/java/실버/_10973.java
@@ -0,0 +1,17 @@
+package 백준.java.실버;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10973 {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+
+    }
+
+    public static List<String>
+}
---
+++ CodingTest/백준/java/실버/_2407.java
@@ -0,0 +1,22 @@
+package 백준.java.실버;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+public class _2407 {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+
+
+    }
+
+    public static dfs()
+}
