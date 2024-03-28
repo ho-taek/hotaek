adfddf4a4fce23e0983ef2cf23dbd28b8d88109b
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-03-28 13:56:03 +0900

    계단오르기
    dp문제!

--- CodingTest/백준/java/실버/_2579.java
+++ CodingTest/백준/java/실버/_2579.java
@@ -27,10 +27,12 @@ public class _2579 {
                 dp[2][2] = dp[2][0];
             }
             if (i >= 3) {
-
+              dp[i][1] = dp[i-1][2] + dp[i][0];
+              dp[i][2] = Math.max(dp[i-2][1], dp[i-2][2]) + dp[i][0];
             }
-
         }
+
+        System.out.println(Math.max(dp[n][1], dp[n][2]));
 
     }
 }
