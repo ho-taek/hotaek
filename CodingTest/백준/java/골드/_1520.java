b452ec5ad786623680102c018986c5d1e23f867f
ho-taek <kht072623@naver.com>
2024-04-07 21:15:51 +0900

    Update _1520.java

--- CodingTest/백준/java/골드/_1520.java
+++ CodingTest/백준/java/골드/_1520.java
@@ -8,8 +8,16 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _1520 {
-    static int[][] graph;
     static int result;
+
+    static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,7 +28,7 @@ public class _1520 {
         int m = Integer.parseInt(st.nextToken());
 
         result = 0;
-        graph = new int[n][m];
+        int[][] graph = new int[n][m];
         for (int i = 0; i < n; i++) {
             StringTokenizer stg = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
@@ -28,36 +36,34 @@ public class _1520 {
             }
         }
 
-        bfs(0, 0, n, m);
+        bfs(new Node(0, 0), n, m, graph);
         System.out.println(result);
     }
 
-    public static void bfs(int startX, int startY, int n, int m) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] { startX, startY });
+    public static void bfs(Node start, int n, int m, int[][] graph) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(start);
 
         int[] dx = { 0, 0, -1, 1 };
         int[] dy = { -1, 1, 0, 0 };
 
         while (!queue.isEmpty()) {
-            int[] q = queue.poll();
+            Node q = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = q[0] + dx[i];
-                int ny = q[1] + dy[i];
+                int nx = q.x + dx[i];
+                int ny = q.y + dy[i];
 
                 if (nx < 0 || nx >= n || ny < 0 || ny >= m)
                     continue;
 
-                if (graph[q[0]][q[1]] > graph[nx][ny]) {
+                if (graph[q.x][q.y] > graph[nx][ny]) {
                     if (nx == (n - 1) && ny == (m - 1)) {
                         result++;
                     }
-                    queue.offer(new int[] { nx, ny });
+                    queue.offer(new Node(nx, ny));
                 }
             }
-
         }
-
     }
 }
