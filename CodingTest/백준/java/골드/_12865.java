9e6b97a510b27897981db1e75a8128589d53e012
ho-taek <kht072623@naver.com>
2024-04-07 22:06:32 +0900

    배낭 문제

---
+++ CodingTest/백준/java/골드/_12865.java
@@ -0,0 +1,56 @@
+package 백준.java.골드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _12865 {
+
+    static class Thing {
+        int weight, value;
+
+        public Thing(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Thing[] things = new Thing[n + 1];
+
+        things[0] = new Thing(0, 0);
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer stg = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(stg.nextToken());
+            int b = Integer.parseInt(stg.nextToken());
+            things[i] = new Thing(a, b);
+        }
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                // 무게가 크면 이전꺼 그대로 가져오기
+                if (things[i].weight > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - things[i].weight] + things[i].value);
+                }
+
+            }
+            System.out.println(Arrays.toString(dp[i]));
+        }
+
+        System.out.println(dp[n][k]);
+
+    }
+}
--- CodingTest/백준/java/골드/_1520.java
+++ CodingTest/백준/java/골드/_1520.java
@@ -9,6 +9,9 @@ import java.util.StringTokenizer;
 
 public class _1520 {
     static int result;
+    static int[][] graph;
+    static int n;
+    static int m;
 
     static class Node {
         int x, y;
@@ -24,8 +27,8 @@ public class _1520 {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         result = 0;
         int[][] graph = new int[n][m];
@@ -36,34 +39,11 @@ public class _1520 {
             }
         }
 
-        bfs(new Node(0, 0), n, m, graph);
+        dfs(new Node(n - 1, m - 1), n, m, graph);
         System.out.println(result);
     }
 
-    public static void bfs(Node start, int n, int m, int[][] graph) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(start);
+    public static void dfs(Node start, int[][] visited) {
 
-        int[] dx = { 0, 0, -1, 1 };
-        int[] dy = { -1, 1, 0, 0 };
-
-        while (!queue.isEmpty()) {
-            Node q = queue.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = q.x + dx[i];
-                int ny = q.y + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-                    continue;
-
-                if (graph[q.x][q.y] > graph[nx][ny]) {
-                    if (nx == (n - 1) && ny == (m - 1)) {
-                        result++;
-                    }
-                    queue.offer(new Node(nx, ny));
-                }
-            }
-        }
     }
 }
