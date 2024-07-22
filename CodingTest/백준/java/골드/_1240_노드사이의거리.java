d481561e6c3fabf192c887dd91dfc0250c754887
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-07-22 09:30:15 +0900

    1240 노드 사이 거리

---
+++ CodingTest/백준/java/골드/_1240_노드사이의거리.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class _1240_노드사이의거리 {
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        
+
+        int[][] graph = new int[n+1][n+1];
+
+        for(int i =1; i < n+1; i++){
+            for(int j = 1; j < n+1; j++){
+                if(i == j) {
+                    graph[i][j] = 0;
+                    continue;
+                }
+                graph[i][j] = 10000;
+            }
+        }
+        
+        for(int i = 0; i < n-1; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            graph[a][b] = t;
+            graph[b][a] = t;
+        }
+
+
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < n+1; j++){
+                for(int k = 1 ; k < n+1; k++){
+                    graph[j][k] = Integer.min(graph[j][k], graph[j][i] + graph[i][k]);
+                }
+            }
+        }
+
+        for(int i = 0 ; i< m; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(graph[a][b]);
+        }
+
+    }
+
+}
