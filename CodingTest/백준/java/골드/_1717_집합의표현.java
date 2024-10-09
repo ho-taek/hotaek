4f857df09612683a211e83facaef9c7d002e0da8
ho-taek <kht072623@naver.com>
2024-10-09 20:06:38 +0900

    Create _1717_집합의표현.java

---
+++ CodingTest/백준/java/골드/_1717_집합의표현.java
@@ -0,0 +1,66 @@
+package 백준.java.골드;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class _1717_집합의표현 {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int type = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (type == 0) {
+                union(a, b);
+                continue;
+            }
+
+            if (find(a) == find(b)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        System.out.println(Arrays.toString(parent));
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x < y) {
+            parent[y] = x;
+        } else {
+            parent[x] = y;
+        }
+
+    }
+
+    public static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+
+    }
+
+}
