8848260e3caa0a9e49f4f2d091e8b242bc28687c
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-11-29 14:05:50 +0900

    Create _1253_좋다.java

---
+++ CodingTest/백준/java/골드/_1253_좋다.java
@@ -0,0 +1,57 @@
+package 백준.java.골드;
+
+
+import java.io.*;
+import java.util.*;
+
+public class _1253_좋다 {
+    
+
+    public static void main(String[] args) throws  IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] graph = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            graph[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(graph);
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                if (left == i) {
+                    left++;
+                    continue;
+                }
+                if (right == i) {
+                    right--;
+                    continue;
+                }
+
+                int sum = graph[left] + graph[right];
+
+                if (sum == graph[i]) { // "좋다" 조건 만족
+                    answer++;
+                    break;
+                } else if (sum < graph[i]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    
+}
