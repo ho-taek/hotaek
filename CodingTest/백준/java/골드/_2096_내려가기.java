ef6aa0fe2e69bbf5db1093305a190ac71015bd5d
ho-taek <kht072623@naver.com>
2025-01-02 14:30:41 +0900

    Update _2096_내려가기.java

--- CodingTest/백준/java/골드/_2096_내려가기.java
+++ CodingTest/백준/java/골드/_2096_내려가기.java
@@ -9,35 +9,37 @@ public class _2096_내려가기 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        int[][][] dp = new int[n][3][2];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 3; i++) {
-            int value = Integer.parseInt(st.nextToken());
-            dp[0][i][0] = value;
-            dp[0][i][1] = value;
+        int[] maxDp = new int[3];
+        int[] minDp = new int[3];
+        int[] tempMax = new int[3];
+        int[] tempMin = new int[3];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] row = new int[3];
+            for (int j = 0; j < 3; j++) {
+                row[j] = Integer.parseInt(st.nextToken());
+            }
+
+            tempMax[0] = row[0] + Math.max(maxDp[0], maxDp[1]);
+            tempMax[1] = row[1] + Math.max(Math.max(maxDp[0], maxDp[1]), maxDp[2]);
+            tempMax[2] = row[2] + Math.max(maxDp[1], maxDp[2]);
+
+            tempMin[0] = row[0] + Math.min(minDp[0], minDp[1]);
+            tempMin[1] = row[1] + Math.min(Math.min(minDp[0], minDp[1]), minDp[2]);
+            tempMin[2] = row[2] + Math.min(minDp[1], minDp[2]);
+
+            System.arraycopy(tempMax, 0, maxDp, 0, 3);
+            System.arraycopy(tempMin, 0, minDp, 0, 3);
         }
 
-        for (int i = 1; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            dp[i][0][0] = Math.max(dp[i - 1][0][0] + a, dp[i - 1][1][0] + a);
-            dp[i][1][0] = Math.max(dp[i][0][0], dp[i - 1][2][0] + b);
-            dp[i][2][0] = Math.max(dp[i - 1][1][0] + c, dp[i - 1][2][0] + c);
+        int maxScore = Math.max(Math.max(maxDp[0], maxDp[1]), maxDp[2]);
+        int minScore = Math.min(Math.min(minDp[0], minDp[1]), minDp[2]);
 
-            dp[i][0][1] = Math.min(dp[i - 1][0][1] + a, dp[i - 1][1][1] + a);
-            dp[i][1][1] = Math.min(dp[i][0][1], dp[i - 1][2][1] + b);
-            dp[i][2][1] = Math.min(dp[i - 1][1][1] + c, dp[i - 1][2][1] + c);
-        }
+        System.out.println(maxScore + " " + minScore);
 
-        int max = Math.max(dp[n - 1][0][0], Math.max(dp[n - 1][1][0], dp[n - 1][2][0]));
-        int min = Math.min(dp[n - 1][0][1], Math.min(dp[n - 1][1][1], dp[n - 1][2][1]));
-
-        System.out.println(max + " " + min);
     }
 
 }
